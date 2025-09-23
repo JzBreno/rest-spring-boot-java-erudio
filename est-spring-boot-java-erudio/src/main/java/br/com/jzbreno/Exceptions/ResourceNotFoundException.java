b8d97486a392dac44f25f8db2d24cdd54fd22e11
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2025-09-22 23:26:00 -0300

    alterando nossas exceptions

--- est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/Exceptions/ResourceNotFoundException.java
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/Exceptions/ResourceNotFoundException.java
@@ -3,11 +3,11 @@ package br.com.jzbreno.Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 //usando para iserir qual status se ira retornar
 //esse cara ira gerar um status code
-public class SumAritmeticException extends RuntimeException {
-    public SumAritmeticException(String message) {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
         super(message);
     }
 }
--- est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/Exceptions/handler/CustomEntityResponseHandler.java
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/Exceptions/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package br.com.jzbreno.Exceptions.handler;
 
 import br.com.jzbreno.Exceptions.ExceptionResponse;
-import br.com.jzbreno.Exceptions.SumAritmeticException;
+import br.com.jzbreno.Exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +11,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-import java.util.IllegalFormatException;
 
 @RestController
 @ControllerAdvice
@@ -37,12 +36,12 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
 
     };
 
-    @ExceptionHandler(SumAritmeticException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception ex, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
 
     };
 

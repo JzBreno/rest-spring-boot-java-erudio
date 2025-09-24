1f79bda084e7bcc57683e81f242a6c6ba12db411
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2025-09-24 00:45:55 -0300

    isnerindo logs no projeto

--- est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/services/PersonServices.java
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/services/PersonServices.java
@@ -3,24 +3,22 @@ package br.com.jzbreno.services;
 import br.com.jzbreno.Exceptions.ResourceNotFoundException;
 import br.com.jzbreno.model.Person;
 import br.com.jzbreno.repository.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Logger;
 
 @Service
 //essa anotacao serve para deixar claro que e uma classe de logica de negocio, e podemos injetala onde precisamos, com  @Autowired ou injecao por construtor(mais recomendado)
 //alias para o anotation component
-
+@Slf4j
 public class PersonServices {
 
 //    private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+//    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
     private final PersonRepository personRepository;
 
     public PersonServices(PersonRepository personRepository) {
@@ -28,23 +26,24 @@ public class PersonServices {
     }
 
     public Person findById(String id){
-        logger.info("Finding person by id");
+        log.info("Finding person by id : " + id);
         return personRepository.findById(Long.parseLong(id)).orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + id));
     }
 
     public List<Person> findAll(){
-        logger.info("Finding all people");
+        log.info("Finding all people");
+        log.info("list of people : " + personRepository.findAll().toString());
         return personRepository.findAll();
     }
 
     public Person create(@NonNull Person person){
-        logger.info("Creating person");
+        log.info("Creating person : " + person.toString());
         personRepository.save(person);
         return person;
     }
 
     public Person updating(Person person){
-        logger.info("Updating person");
+        log.info("Updating person : " + person.toString() );
         Person personUpdate = findById(person.getId().toString());
         personUpdate.setFirstName(person.getFirstName());
         personUpdate.setLastName(person.getLastName());
@@ -53,7 +52,7 @@ public class PersonServices {
     }
 
     public void deleteById(String id){
-        logger.info("Deleting person");
+        log.info("Deleting person : " + id);
         personRepository.deleteById(Long.parseLong(id));
     }
 

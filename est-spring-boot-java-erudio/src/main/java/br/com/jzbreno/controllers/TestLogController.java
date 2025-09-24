d2a953081f2462fd056a49936eab4c9e6ae9923b
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2025-09-24 00:44:35 -0300

    integrando o @data para melhorar leitura

---
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/controllers/TestLogController.java
@@ -0,0 +1,4 @@
+package br.com.jzbreno.controllers;
+
+public class TestLogController {
+}
--- est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/model/Person.java
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/model/Person.java
@@ -1,6 +1,8 @@
 package br.com.jzbreno.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.io.Serializable;
@@ -8,6 +10,8 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "person")
+@Slf4j
+@Data
 public class Person implements Serializable {
 
     private final static long serialVersionUID = 1L;
@@ -23,73 +27,4 @@ public class Person implements Serializable {
 //    @Enumerated(EnumType.STRING)
     private String gender;
 
-    public Person() {}
-
-    public Person(Long id, String firstName, String lastName, String gender) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", gender='" + gender + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Person person)) return false;
-        return Objects.equals(getId(), person.getId()) && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getGender(), person.getGender());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getGender());
-    }
-
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }

3e6c67bb250844f4e72457bdb2943f44fa17262b
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2025-11-10 21:04:03 -0300

    adicionando swagger ao endpoint books

--- est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/controllers/BookController.java
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/controllers/BookController.java
@@ -2,7 +2,7 @@ package br.com.jzbreno.controllers;
 
 import br.com.jzbreno.model.DTO.BookDTO;
 import br.com.jzbreno.services.BookServices;
-import lombok.extern.slf4j.Slf4j;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +12,8 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("/book")
-@Slf4j
-public class BookController {
+@Tag(name = "Book", description = "Enpoint for managing Book, version 2 with Content Negotiation and Others tecnologies")
+public class BookController implements BookControllerDoc {
 
     private BookServices bookServices;
 
@@ -29,33 +29,38 @@ public class BookController {
     }
 
     @GetMapping(value = "/findAll"
-                , produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+            , produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public ResponseEntity<List<BookDTO>> findAllBooks() {
         List<BookDTO> bookList = bookServices.findAllBooks();
         return bookList != null ? ResponseEntity.ok(bookList) : ResponseEntity.notFound().build();
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE, MediaType.APPLICATION_XML_VALUE},
-    produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public ResponseEntity<BookDTO> saveBook(@RequestBody BookDTO bookDTO) {
         return ResponseEntity.ok().body(bookServices.createBook(bookDTO));
     }
 
     @PostMapping(value = "/create",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}    )
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public ResponseEntity<BookDTO> createBook(@RequestBody BookDTO bookDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(bookServices.createBook(bookDTO));
     }
 
     @PutMapping(value = "/update",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE, MediaType.APPLICATION_XML_VALUE},
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}    )
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
+    @Override
     public ResponseEntity<BookDTO> updateBook(@RequestBody BookDTO bookDTO) {
         return ResponseEntity.ok().body(bookServices.updating(bookDTO));
     }
 
     @DeleteMapping(value = "/{id}")
+    @Override
     public ResponseEntity<Void> deleteBook(@PathVariable(name = "id") String id) {
         bookServices.deleteById(Long.valueOf(id));
         return ResponseEntity.noContent().build();

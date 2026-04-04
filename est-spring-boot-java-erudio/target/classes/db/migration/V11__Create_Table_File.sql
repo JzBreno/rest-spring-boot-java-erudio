41d8ad336a0de56f565040c667b86ba3cf413eb9
JzBreno <jose.breno.sousa61@aluno.ifce.edu.brm>
2026-04-04 01:32:56 -0300

    chore: ignora pasta target e arquivos de configuração da IDE

--- est-spring-boot-java-erudio/target/classes/db/migration/V11__Create_Table_File.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE files (
-                       id BIGINT AUTO_INCREMENT NOT NULL,
-                       path VARCHAR(255) NOT NULL,
-                       filesize BIGINT NOT NULL,
-                       filename VARCHAR(255) NOT NULL,
-                       CONSTRAINT pk_files PRIMARY KEY (id)
-);

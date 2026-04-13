ba610abb48c6c0b09282958c9b5b5249a68e8199
jzbreno <jose.breno.sousa61@aluno.ifce.edu.br>
2026-04-13 19:08:46 -0300

    adicionando exporter xlsx jasper

---
+++ est-spring-boot-java-erudio/src/main/java/br/com/jzbreno/file/exporter/impl/XlsxJasperExporter.java
@@ -0,0 +1,55 @@
+package br.com.jzbreno.file.exporter.impl;
+
+import br.com.jzbreno.file.exporter.contract.FileExporter;
+import br.com.jzbreno.model.DTO.PersonDTO;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class XlsxJasperExporter implements FileExporter {
+    @Override
+    public boolean canHandle(String name) {
+        return name.equalsIgnoreCase("");
+    }
+
+    @Override
+    public Resource exportFile(List<PersonDTO> people) throws Exception {
+
+        InputStream jasperStream = getClass().getResourceAsStream("/reports/people_report.jasper");
+
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(people);
+
+        Map<String, Object> parameters = new HashMap<>();
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperStream, parameters, dataSource);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        JRXlsxExporter exporter = new JRXlsxExporter();
+
+        exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+        exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(outputStream));
+
+        SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+        configuration.setOnePagePerSheet(false);
+        configuration.setRemoveEmptySpaceBetweenRows(true);
+        configuration.setDetectCellType(true);
+        configuration.setWhitePageBackground(false);
+
+        exporter.setConfiguration(configuration);
+        exporter.exportReport();
+
+        return new ByteArrayResource(outputStream.toByteArray());
+    }
+}
